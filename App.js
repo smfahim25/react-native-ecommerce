ff00c557a917cdf0a7eece95ffc41268df83ee25
smfahim25 <smfahimhossen@gmail.com>
2022-06-09 19:37:38 +0600

    images & database added

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Fahim</Text>
+      <Text>Work</Text>
       <Text>Fahim</Text>
       <StatusBar style="auto" />
     </View>
